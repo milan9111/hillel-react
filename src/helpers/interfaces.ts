20ced557d063bf640eb8f39f46be484622a997c0
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-12-18 00:10:55 +0200

    homework8.2 refactoring interfaces

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -11,34 +11,34 @@ export interface ProductsType  {
     }
 }
 
-export interface AppProps {
-    addProduct: (event: React.MouseEvent<HTMLButtonElement>) => void;
+export interface AppProps extends ProductListContainerProps, BasketContainerProps, HeaderContainerProps  {
+     
+}
+
+export interface HeaderProps extends HeaderContainerProps {
+    countTypesProductsInBasket: number;
+}
+
+export interface ProductListProps extends ProductListContainerProps {
+    id: number; 
+    image: string;
+    title: string;
+    price: number;
+    selectedIdProduct: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export interface BasketContainerProps {
     removeProduct: (event: React.MouseEvent<HTMLButtonElement>) => void;
     removeOneItem: (event: React.MouseEvent<HTMLButtonElement>) => void;
     addOneItem: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    onSearchProducts: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export interface HeaderContainerProps {
     onSearchProducts: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export interface HeaderProps {
-    countTypesProductsInBasket: number;
-    onSearchProducts: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
 export interface ProductListContainerProps {
     addProduct: (event: React.MouseEvent<HTMLButtonElement>) => void;
-}
-
-export interface ProductListProps {
-    id: number; 
-    image: string;
-    title: string;
-    price: number;
-    addProduct: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    selectedIdProduct: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export interface CustomModalProps {
@@ -52,8 +52,3 @@ export interface ProductDetailsProps {
 }
 
 
-export interface BasketContainerProps {
-    removeProduct: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    removeOneItem: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    addOneItem: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  }
